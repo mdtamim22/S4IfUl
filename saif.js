9560f4dd8ce7215cafc7b4575a988b58557818e9
mdtamim22 <md1881311@gmail.com>
2025-01-13 17:36:44 +0600

    Update and rename scripts/cmds/bayjid.js to saif.js

--- saif.js
+++ saif.js
@@ -1,6 +1,6 @@
 module.exports = {
  config: {
-	 name: "bayjid",
+	 name: "saif",
 	 version: "1.0",
 	 author: "AceGun",
 	 countDown: 5,
@@ -15,7 +15,7 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "bayjid") {
  return message.reply({
  body: " 「❥︎----ღ᭄_ᴬˢˢᴬᴸᴬᴹᴼᴸᴬᴵᴷᵁᴹ ..\n❥︎----ღ᭄_  ᴮᴿᴼᵀᴴᴱᴿ❞࿐.🌴.\n❥ ᴍᴏʜᴀᴍᴍᴀᴅ ʙᴀʏᴊɪᴅ\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝐌𝐎𝐇𝐀𝐌𝐌𝐀𝐃 𝐁𝐀𝐘𝐉𝐈𝐃」",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/XlyIOMR.mp4")
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/ZHhtVqp.mp4")
  });
  }
  }
